ed220ca5d3f4a19a17d07f0cbb85b00d41dde65f
MD Sanju <mdsanju387@gmail.com>
2022-09-13 06:30:16 +0600

    count functionality added

--- src/Pages/Home/Count/Count.js
+++ src/Pages/Home/Count/Count.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaUsers } from "react-icons/fa";
 import { BiCube, BiReceipt } from "react-icons/bi";
 import { CgOrganisation } from "react-icons/cg";
@@ -10,32 +10,81 @@ import {
 } from "../../styles/Count.styles";
 
 const Count = () => {
+  const [mouseOver, setMouseOver] = useState(false);
+
+  useEffect(() => {
+    const counters = document.querySelectorAll(".counter");
+    const speed = 500; // The lower the slower
+
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        // Lower inc to slow and higher to slow
+        const inc = target / speed;
+
+        // console.log(inc);
+        // console.log(count);
+
+        // Check if target is reached
+        if (count < target) {
+          // Add inc to count and output in counter
+          counter.innerText = parseInt(count + inc);
+          // Call function every ms
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      if (mouseOver === true) {
+        updateCount();
+      }
+    });
+  }, [mouseOver]);
+
+  const handleMouseOver = () => {
+    setMouseOver(true);
+  };
+
   return (
-    <CountContainer>
+    <CountContainer onMouseOver={handleMouseOver}>
       <CountRow>
         <CountRowItem>
-          <h2>581,237</h2>
+          <h2 className="counter" data-target="581237">
+            0
+          </h2>
           <CountRowName>
             <FaUsers color="#212529" size={20} />
             <span>Visitors</span>
           </CountRowName>
         </CountRowItem>
         <CountRowItem>
-          <h2>500+</h2>
+          <h2>
+            <span className="counter" data-target="1000">
+              0
+            </span>
+            +
+          </h2>
           <CountRowName>
             <BiCube color="#212529" size={20} />
             <span>Products</span>
           </CountRowName>
         </CountRowItem>
         <CountRowItem>
-          <h2>230</h2>
+          <h2 className="counter" data-target="500">
+            0
+          </h2>
           <CountRowName>
             <CgOrganisation color="#212529" size={20} />
             <span>Organizations</span>
           </CountRowName>
         </CountRowItem>
         <CountRowItem>
-          <h2>20,119</h2>
+          <h2 className="counter" data-target="20119">
+            0
+          </h2>
           <CountRowName>
             <BiReceipt color="#212529" size={20} />
             <span>Orders</span>
